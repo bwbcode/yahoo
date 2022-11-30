9fd347e5cc3bb29c055ddae653271a8dc94adbe1
bwbcode <bwalkerbr@gmail.com>
2022-11-29 19:40:08 -0700

    succesfully querying game_keys

---
+++ get_game_keys.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+import json
+
+from yfpy import Data
+from yfpy.query import YahooFantasySportsQuery
+
+"""
+Example public Yahoo league URL: "https://archive.fantasysports.yahoo.com/nfl/2014/729259"
+
+Example vars using public Yahoo leagues still require auth through a personal Yahoo account: see README.md
+"""
+
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# ENVIRONMENT SETUP # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# load .env file in order to read local environment variables
+load_dotenv()
+
+# set directory location of private.json for authentication
+auth_dir = Path(__file__).parent
+
+# set target directory for data output
+data_dir = Path(__file__).parent / "output"
+
+# create YFPY Data instance for saving/loading data
+data = Data(data_dir)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# VARIABLE SETUP  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# set desired Yahoo Fantasy Sports game code
+# game_code = "nfl"  # NFL
+game_code = "nhl"  # NHL
+
+# set desired league ID (see README.md for finding value)
+#league_id = "3712"
+league_id = "13405"
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# QUERY SETUP # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# configure the Yahoo Fantasy Sports query (change all_output_as_json_str=True if you want to output JSON strings)
+yahoo_query = YahooFantasySportsQuery(
+    auth_dir,
+    league_id,
+    game_code=game_code,
+    offline=False,
+    all_output_as_json_str=False,
+    consumer_key=os.environ["YFPY_CONSUMER_KEY"],
+    consumer_secret=os.environ["YFPY_CONSUMER_SECRET"],
+    browser_callback=True
+)
+
+for row in yahoo_query.get_all_yahoo_fantasy_game_keys():
+    print(type(row['game']))
