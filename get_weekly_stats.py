598863bcc6d8be84a809e6499935d4d7084f7535
bwbcode <bwalkerbr@gmail.com>
2022-11-29 22:08:00 -0700

    creating dictionary of teams with weekly results

--- get_weekly_stats.py
+++ get_weekly_stats.py
@@ -1,21 +1,8 @@
-# -*- coding: utf-8 -*-
-
 import os
 from pathlib import Path
-
 from dotenv import load_dotenv
 
-import json
-
-from yfpy import Data
 from yfpy.query import YahooFantasySportsQuery
-
-"""
-Example public Yahoo league URL: "https://archive.fantasysports.yahoo.com/nfl/2014/729259"
-
-Example vars using public Yahoo leagues still require auth through a personal Yahoo account: see README.md
-"""
-
 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -31,9 +18,6 @@ auth_dir = Path(__file__).parent
 # set target directory for data output
 data_dir = Path(__file__).parent / "output"
 
-# create YFPY Data instance for saving/loading data
-data = Data(data_dir)
-
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # VARIABLE SETUP  # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -41,43 +25,14 @@ data = Data(data_dir)
 # set desired season year
 season = 2022
 
-# set desired week
-# chosen_week = 1
-
 # set desired Yahoo Fantasy Sports game code
-# game_code = "nfl"  # NFL
 game_code = "nhl"  # NHL
 
-# set desired league ID (see README.md for finding value)
+# set desired league ID (see README.md for finding value) - REQUIRED
 league_id = "3712"
 
-# set desired date
-# chosen_date = "2013-04-15"  # NHL - 2013 (for 2012)
-# chosen_date = "2013-04-16"  # NHL - 2013
-# chosen_date = "2021-10-25"  # NHL - 2021
-# chosen_date = "2021-04-01"  # MLB - 2021
-# chosen_date = "2022-04-10"  # MLB - 2022
-
-# set desired Yahoo Fantasy Sports game ID (see the get_all_yahoo_fantasy_game_keys query to retrieve values)
-game_id = 419  # NFL - 2014
-
-
-# set desired Yahoo Fantasy Sports game key (see the get_all_yahoo_fantasy_game_keys query to retrieve values)
-game_key = "419"  # NFL - 2014
-
-
-# set desired team ID within desired league
-# team_id = 1  # NFL
-
-# set desired team name within desired league
-# team_name = "Legion"  # NFL
-
-# set desired team ID within desired league
-# player_id = 7200  # NFL: Aaron Rodgers - 2020/2021
-
-# set the maximum number players you wish the get_league_players query to retrieve
-#league_player_limit = 101
-# league_player_limit = 2610
+# set desired Yahoo Fantasy Sports game ID - REQUIRED
+game_id = 419 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # QUERY SETUP # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -87,8 +42,8 @@ game_key = "419"  # NFL - 2014
 yahoo_query = YahooFantasySportsQuery(
     auth_dir,
     league_id,
-    game_id=game_id,
     game_code=game_code,
+    game_id=game_id,
     offline=False,
     all_output_as_json_str=False,
     consumer_key=os.environ["YFPY_CONSUMER_KEY"],
@@ -96,11 +51,6 @@ yahoo_query = YahooFantasySportsQuery(
     browser_callback=True
 )
 
-# Manually override league key for example code to work
-# yahoo_query.league_key = f"{game_id}.l.{league_id}"
-
-# Manually override player key for example code to work
-# player_key = f"{game_id}.p.{player_id}"
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # RUN QUERIES # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
